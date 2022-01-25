b3e600e26824c41dec07c3f1dd5647167306751f
Sawyer Knowles <skdnowle@mtu.edu>
2022-01-25 13:24:42 -0500

    Added player movement and idle animations

--- CAZ/Assets/RPG/Scripts/Movement/PlayerController.cs
+++ CAZ/Assets/RPG/Scripts/Movement/PlayerController.cs
@@ -5,25 +5,39 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    private Rigidbody2D rb;
+    private Rigidbody2D rb; // player's Rigidbody2D
+    private Animator anim; // player's Animatior
 
     [SerializeField]
-    private float speed;
+    private float speed; // Movement speed of player ~200 seems well paced
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>(); // player's Rigidbody2D
+        anim = GetComponent<Animator>(); // player's Animatior
     }
 
     // Update is called once per frame
     void Update()
     {
-        //rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * speed * Time.deltaTime;
+        
     }
 
     private void FixedUpdate()
     {
+        // Get input and move player
         rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * speed * Time.deltaTime;
+
+        // Set animations for player's direction
+        anim.SetFloat("moveX", rb.velocity.x);
+        anim.SetFloat("moveY", rb.velocity.y);
+
+        // Control idle animation direction
+        if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+        {
+            anim.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+            anim.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+        }
     }
 }
