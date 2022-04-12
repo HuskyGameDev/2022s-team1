ace14f305e82494286eb62c7c3c9acb1f069e387
Adam Johnson <70647530+GeneralofCats@users.noreply.github.com>
2022-04-12 18:56:35 -0400

    Dialogue Scripts

---
+++ CAZ/Assets/RPG/Scripts/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : Interactable
+{
+    public DialogueBase DB;
+
+    public override void Interact()
+    {
+        DialogueManager.instance.EnqueueDialogue(DB);
+    }
+}
